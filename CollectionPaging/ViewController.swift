6e7c99dce0670bde9fe3da3bfab73bb11bd79819
sanjay kumar chaurasia <sanjay@sanjays-mac-mini.local>
2018-04-22 10:33:46 +0530

    50% animation

--- CollectionPaging/ViewController.swift
+++ CollectionPaging/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import FAPaginationLayout
 
 class ViewController: UIViewController {
     @IBOutlet weak var collectionView:UICollectionView!
+    @IBOutlet weak var ht_const: NSLayoutConstraint!
+    var value = 0
+    let ht = UIScreen.main.bounds.height
+    let width = UIScreen.main.bounds.width
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
@@ -25,6 +30,28 @@ class ViewController: UIViewController {
     deinit{
         
         print("in deinit method")
+    }
+    
+    @IBAction func test(_ sender: UIBarButtonItem) {
+        value += 1
+        if(value%2 == 0){
+            self.ht_const.constant = ht-64
+        }
+        else{
+            self.ht_const.constant = ht/2-64
+        }
+       
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        let layout = collectionView.collectionViewLayout as! FAPaginationLayout
+        layout.itemSize =  CGSize(width: width, height: ht-50)
+        layout.minimumInteritemSpacing = 0
+        layout.minimumLineSpacing = 0
+        
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        collectionView.collectionViewLayout = layout
     }
 }
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
